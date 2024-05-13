b2d3bc9da4070fcdf8a13418da2e0ed4336c8432
Tyler Odo <tyler.odo@gmail.com>
2024-05-13 15:27:30 -0700

    added all.css file which contains general purpose classes and variables

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,9 +3,11 @@
 import React from 'react'
 
 const Home = () => {
-    return (
-        <div>Home</div>
-    )
+  return (
+    <div class="bg-black">
+      
+    </div>
+  )
 }
 
 export default Home

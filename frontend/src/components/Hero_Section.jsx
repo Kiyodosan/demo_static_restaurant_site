98f23de74f731aa2f53c860875d212e27d57e2ff
Kiyodo <tyler.odo@gmail.com>
2024-05-19 15:45:44 -0700

    Update and rename hero_section.jsx to Hero_Section.jsx

---
+++ frontend/src/components/Hero_Section.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero_Section = () => {
+  return (
+    <div>Hero_Section</div>
+  )
+}
+
+export default Hero_Section
--- frontend/src/components/hero_section.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const hero_section = () => {
-  return (
-    <div>hero_section</div>
-  )
-}
-
-export default hero_section

139a7c979922585bd66f9a445f08ae3ad8294de9
ruokmain <anthony.hoang1991@gmail.com>
2024-05-19 15:48:01 -0700

    navbar setup complete

--- frontend/src/components/hero_section.jsx
+++ frontend/src/components/hero_section.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const hero_section = () => {
+const Hero_Section = () => {
   return (
     <div>hero_section</div>
   )
 }
 
-export default hero_section
+export default Hero_Section

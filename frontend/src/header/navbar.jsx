996f79d8da9dc35a0015194812f391414fae2187
ruokmain <anthony.hoang1991@gmail.com>
2024-05-19 15:19:02 -0700

    initial commit

--- frontend/src/header/navbar.jsx
+++ frontend/src/header/navbar.jsx
@@ -1,8 +1,21 @@
+import '../assets/css/index.css'
 import React from 'react'
+import { useState } from 'react-dom'
+
 
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <div>
+
+      <header className='header' id='header'>
+        <a href='#' class='header_mask'></a>
+        <div className='container'>
+          <p>Gordon Ramsay Restaurants</p>
+        </div>
+      </header>
+
+
+    </div>
   )
 }
 

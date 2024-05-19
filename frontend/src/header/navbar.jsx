9b03267caebd70b535e9720d5aadf1149c88896f
ruokmain <anthony.hoang1991@gmail.com>
2024-05-19 15:39:21 -0700

    navbar initial setup

--- frontend/src/header/navbar.jsx
+++ frontend/src/header/navbar.jsx
@@ -3,15 +3,63 @@ import React from 'react'
 import { useState } from 'react-dom'
 
 
-const navbar = () => {
+const NavBar = () => {
   return (
     <div>
 
       <header className='header' id='header'>
-        <a href='#' class='header_mask'></a>
-        <div className='container'>
-          <p>Gordon Ramsay Restaurants</p>
+        <div>
+          <a href='#' class='header_mask'></a>
+          <div className='container'>
+            <p>Gordon Ramsay Restaurants</p>
+          </div>
         </div>
+
+        <div className='container'>
+          <div>
+            <div className='header_logo'>
+              <a href='#'>
+                <span>
+                  Hell's Kitchen
+                </span>
+              </a>
+            </div>
+          </div>
+
+
+          <div>
+            <div className='header_book'>
+              <ul>
+                <li>
+                  <a>
+                    <span>Book</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div>
+            <div className='header_menu'>
+              <ul>
+                <li>
+                  <a>Venues</a>
+                </li>
+                <li>
+                  <a>Menus</a>
+                </li>
+                <li>
+                  <a>Private Dining</a>
+                </li>
+                <li>
+                  <a>Gift Cards</a>
+                </li>
+              </ul>
+
+            </div>
+          </div>
+        </div>
+
       </header>
 
 
@@ -19,4 +67,4 @@ const navbar = () => {
   )
 }
 
-export default navbar
+export default NavBar

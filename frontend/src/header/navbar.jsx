634ea544822d94731600e89ee6e16633e57c2c28
ruokmain <anthony.hoang1991@gmail.com>
2024-05-21 13:30:37 -0700

    added some css styling to header

--- frontend/src/header/navbar.jsx
+++ frontend/src/header/navbar.jsx
@@ -1,4 +1,5 @@
 import '../assets/css/index.css'
+import '../assets/css/nav_bar.css'
 import React from 'react'
 import { useState } from 'react-dom'
 
@@ -8,15 +9,19 @@ const NavBar = () => {
     <div>
 
       <header className='header' id='header'>
-        <div>
+        <div className='flex flex-wrap'>
           <a href='#' class='header_mask'></a>
           <div className='container'>
-            <p>Gordon Ramsay Restaurants</p>
+            <div>
+              <div className='header-col'>
+                <p>Gordon Ramsay Restaurants</p>
+              </div>
+            </div>
           </div>
         </div>
 
         <div className='container'>
-          <div>
+          <div className='flex flex-wrap'>
             <div className='header_logo'>
               <a href='#'>
                 <span>
@@ -29,7 +34,7 @@ const NavBar = () => {
 
           <div>
             <div className='header_book'>
-              <ul>
+              <ul className='no-bullets'>
                 <li>
                   <a>
                     <span>Book</span>
@@ -39,22 +44,25 @@ const NavBar = () => {
             </div>
           </div>
 
-          <div>
-            <div className='header_menu'>
-              <ul>
-                <li>
-                  <a>Venues</a>
-                </li>
-                <li>
-                  <a>Menus</a>
-                </li>
-                <li>
-                  <a>Private Dining</a>
-                </li>
-                <li>
-                  <a>Gift Cards</a>
-                </li>
-              </ul>
+          <div className='flex-row flex-wrap'>
+            <div className='header-menu'>
+              <div className='header-flex-col'>
+                <ul className='no-bullets'>
+                  <li>
+                    <a>Venues</a>
+                  </li>
+                  <li>
+                    <a>Menus</a>
+                  </li>
+                  <li>
+                    <a>Private Dining</a>
+                  </li>
+                  <li>
+                    <a>Gift Cards</a>
+                  </li>
+                </ul>
+
+              </div>
 
             </div>
           </div>

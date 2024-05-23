679b5c723091dccfb5c28d54116fa4e68dc60ab2
Kiyodo <tyler.odo@gmail.com>
2024-05-22 17:13:30 -0700

    added food card component to Menu.jsx & implemented food cards to replace large div sections

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -3,83 +3,29 @@ import '../assets/css/card_food.css'
 
 import React from 'react'
 
+import Card_Food from '../components/Card_Food'
+
 const Menu = () => {
   return (
     <div className="flex flex-col w-100vw items-center">
       <section>
         <h1 className="py-1 font-size-2p5">Appetizers</h1>
-        <div className="card_food_container">
-          <div className="card">
-            <div className="card-image">
-              <img src="https://placehold.co/500x500" alt="Delicious Food" />
-            </div>
-            <div className="card-content">
-              <h2>Delicious Pizza</h2>
-              <p>A delightful blend of cheese, tomato, and pepperoni.</p>
-              <div className="card-footer">
-                <span className="price">$12.99</span>
-                <button className="order-button">Order Now</button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Card_Food />
       </section>
 
       <section>
         <h1 className="py-1 font-size-2p5">Entrees</h1>
-        <div className="card_food_container">
-          <div className="card">
-            <div className="card-image">
-              <img src="https://placehold.co/500x500" alt="Delicious Food" />
-            </div>
-            <div className="card-content">
-              <h2>Delicious Pizza</h2>
-              <p>A delightful blend of cheese, tomato, and pepperoni.</p>
-              <div className="card-footer">
-                <span className="price">$12.99</span>
-                <button className="order-button">Order Now</button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Card_Food />
       </section>
 
       <section>
         <h1 className="py-1 font-size-2p5">Beverages</h1>
-        <div className="card_food_container">
-          <div className="card">
-            <div className="card-image">
-              <img src="https://placehold.co/500x500" alt="Delicious Food" />
-            </div>
-            <div className="card-content">
-              <h2>Delicious Pizza</h2>
-              <p>A delightful blend of cheese, tomato, and pepperoni.</p>
-              <div className="card-footer">
-                <span className="price">$12.99</span>
-                <button className="order-button">Order Now</button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Card_Food />
       </section>
 
       <section>
         <h1 className="py-1 font-size-2p5">Desserts</h1>
-        <div className="card_food_container">
-          <div className="card">
-            <div className="card-image">
-              <img src="https://placehold.co/500x500" alt="Delicious Food" />
-            </div>
-            <div className="card-content">
-              <h2>Delicious Pizza</h2>
-              <p>A delightful blend of cheese, tomato, and pepperoni.</p>
-              <div className="card-footer">
-                <span className="price">$12.99</span>
-                <button className="order-button">Order Now</button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Card_Food />
       </section>
     </div>
   )

6d6ea0261a37f8ade6ce06e4164b6e8e5203cc9b
ruokmain <anthony.hoang1991@gmail.com>
2024-06-18 02:59:57 -0700

    completed nav-bar

--- frontend/src/header/navbar.jsx
+++ frontend/src/header/navbar.jsx
@@ -1,76 +1,50 @@
-import '../assets/css/index.css'
-import '../assets/css/nav_bar.css'
 import React from 'react'
 import { useState } from 'react-dom'
 import OpenModalButton from '../components/Open_Modal_Button'
 import Booking from '../pages/Booking'
+import '../assets/css/index.css'
+import '../assets/css/nav_bar.css'
 
 
 const NavBar = () => {
   return (
     <div>
 
-      <header className='header' id='header'>
-        <div className='flex flex-wrap'>
-          <a href='#' class='header_mask'></a>
-          <div className='container'>
-            <div>
-              <div className='header-col'>
-                <p>Gordon Ramsay Restaurants</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className='container'>
-          <div className='flex flex-wrap'>
-            <div className='header_logo'>
-              <a href='#'>
-                <span>
-                  Hell's Kitchen
-                </span>
-              </a>
-            </div>
-          </div>
-
+      <header className='header'>
+        <div className='flex content-between'>
 
           <div>
-            <div className='header_book'>
-              <ul className='no-bullets'>
-                <li>
-                  <OpenModalButton 
-                  buttonText="Bookings"
-                  modalComponent={<Booking />}
-                  />
-                </li>
-              </ul>
-            </div>
+            <p className='home-page'>Gordon Ramsay Restaurants</p>
+            <a href='#' className='block py-1'>
+              <span>
+                Hell's Kitchen
+              </span>
+            </a>
           </div>
 
-          <div className='flex-row flex-wrap'>
-            <div className='header-menu'>
-              <div className='header-flex-col'>
-                <ul className='no-bullets'>
-                  <li>
-                    <a>Venues</a>
-                  </li>
-                  <li>
-                    <a>Menus</a>
-                  </li>
-                  <li>
-                    <a>Private Dining</a>
-                  </li>
-                  <li>
-                    <a>Gift Cards</a>
-                  </li>
-                </ul>
-
-              </div>
-
+          <div className='flex flex-wrap py-1'>
+            <ul className='nav-items flex flex-wrap no-bullets'>
+              <li>
+                <a className='color-white'>Venues</a>
+              </li>
+              <li>
+                <a>Menus</a>
+              </li>
+              <li>
+                <a>Private Dining</a>
+              </li>
+              <li>
+                <a>Gift Cards</a>
+              </li>
+            </ul>
+            <div className='book-button'>
+              <OpenModalButton
+                buttonText="Book"
+                className="book-button"
+                modalComponent={<Booking />}
+              />
             </div>
           </div>
-
-
         </div>
 
       </header>

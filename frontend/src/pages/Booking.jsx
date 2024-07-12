6e68c5ef1c43dc5639428f755658f3927ecf64e5
ruokmain <anthony.hoang1991@gmail.com>
2024-07-12 12:51:54 -0700

    route set-up complete

--- frontend/src/pages/Booking.jsx
+++ frontend/src/pages/Booking.jsx
@@ -1,15 +1,15 @@
-
+import "../assets/css/booking.css"
 
 const Booking = () => {
   return (
-    <div style={{ color: "black" }}>
-      <h1>TEST</h1>
-      <form onSubmit={() => { alert("Feature to be added in a later update") }}>
-          <label style={{ color: "blue" }}>
-            Enter your reservation
-            <input style={{ background: "black", color: "red" }} type="text" />
-          </label>
-        <button type="Submit">Submit</button>
+    <div className="booking-container">
+      <h1 style={{ color: "blue" }}>TEST</h1>
+      <h2 style={{ color: "blue" }}>Another TEST</h2>
+      <form onSubmit={() => alert("Functionality to be added in a later update")}>
+        <h3 style={{ color: "blue" }}>Test</h3>
+        <label htmlFor="book" style={{color: "red"}}>Add your date</label>
+        <input type="text" id="book" style={{ background: "blue" }}></input>
+        <button>Submit</button>
       </form>
     </div>
   )
